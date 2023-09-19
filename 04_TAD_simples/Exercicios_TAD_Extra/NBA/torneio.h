d3c93390377a769efa955ba8a7e703dae3833783
Louzadino <guilhermelouzada852@gmail.com>
2023-09-18 21:34:34 -0300

    NBA 1.0

---
+++ 04_TAD_simples/Exercicios_TAD_Extra/NBA/torneio.h
@@ -0,0 +1,14 @@
+#ifndef _TORNEIO_H
+#define _TORNEIO_H
+
+#include "constantes.h"
+#include "franquia.h"
+
+typedef struct{
+    tFranquia franquias[MAX_FRANQUIAS];
+    int qtdFranquias;
+    
+} tTorneio;
+
+
+#endif
